eb9d76209c226c74b8ef80ed898889c2e507adea
Daniel Ideriba <danielideriba86@gmail.com>
2022-06-08 13:14:44 -0400

    first commit

---
+++ app/src/main/java/com/example/analyticskithuawei/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.analyticskithuawei
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.huawei.hms.analytics.HiAnalytics;
+import com.huawei.hms.analytics.HiAnalyticsTools;
+
+
+//import com.google.firebase.messaging.FirebaseMessagingService
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
